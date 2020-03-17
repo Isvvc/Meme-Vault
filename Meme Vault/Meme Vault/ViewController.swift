afa083c18843b645c1a2831704d618cbb9f39f73
Isvvc <lyons@tuta.io>
2020-03-16 22:25:53 -0600

    Create blank Xcode project

---
+++ Meme Vault/Meme Vault/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meme Vault
+//
+//  Created by Isaac Lyons on 3/16/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
