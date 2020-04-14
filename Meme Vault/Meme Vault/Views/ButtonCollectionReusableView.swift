d91c037eca4409cf1bc2d70424a9e0b38bb9b129
Isvvc <lyons@tuta.io>
2020-04-14 15:21:18 -0600

    Replace add action bar button item with button below actions

---
+++ Meme Vault/Meme Vault/Views/ButtonCollectionReusableView.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonCollectionReusableView.swift
+//  Meme Vault
+//
+//  Created by Isaac Lyons on 4/14/20.
+//  Copyright © 2020 Isaac Lyons. All rights reserved.
+//
+
+import UIKit
+
+class ButtonCollectionReusableView: UICollectionReusableView {
+    @IBOutlet weak var button: UIButton!
+}
